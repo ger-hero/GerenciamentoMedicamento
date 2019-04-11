7db8bd62aa297b3ba76c5b95ca3b00c55db22c5f
Gabriellpa <gabriellpantunes@gmail.com>
2019-04-11 14:56:08 -0300

    add comentarios

--- src/com/gerenciamentomedicamento/controller/PacienteController.java
+++ src/com/gerenciamentomedicamento/controller/PacienteController.java
@@ -5,17 +5,30 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import com.gerenciamentomedicamento.model.Conexao;
 import com.gerenciamentomedicamento.model.Paciente;
 
+/**
+ * Classe Paciente
+ * @author JonasFragoso
+ *
+ */
 public class PacienteController {
-	
+
+/**
+ * Pesquisa Paciente sem filtro
+ * @return Lista de todos pacientes da tabela PACIENTE
+ */
 public List<Paciente> buscarPaciente(){
 	return buscarPaciente(new Paciente());
 }
-	
+
+/**
+ * Pesquisa Paciente com filtro
+ * @param paciente Filtro
+ * @return Retorna lista dos paicentes 
+ */
 public List<Paciente> buscarPaciente(Paciente paciente) {
-	List<Paciente> pacientes = new ArrayList();
+	List<Paciente> pacientes = new ArrayList<Paciente>();
 	ResultSet resultSet;
 	resultSet = paciente.retornaPacientes();
 	int idPaciente;
