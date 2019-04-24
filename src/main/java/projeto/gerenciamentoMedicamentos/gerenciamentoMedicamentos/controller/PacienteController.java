8a65749c6a28426b0ea12f70efe8f42df64542ce
Lucas Hedlund <lucas.amelio.hedlund@ibm.com>
2019-04-24 14:24:44 -0300

    Removido imports desnecessários de PacienteController.java

--- src/main/java/projeto/gerenciamentoMedicamentos/gerenciamentoMedicamentos/controller/PacienteController.java
+++ src/main/java/projeto/gerenciamentoMedicamentos/gerenciamentoMedicamentos/controller/PacienteController.java
@@ -5,9 +5,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import projeto.gerenciamentoMedicamentos.gerenciamentoMedicamentos.model.Medicamento;
 import projeto.gerenciamentoMedicamentos.gerenciamentoMedicamentos.model.Paciente;
-import projeto.gerenciamentoMedicamentos.gerenciamentoMedicamentos.repositories.MedicamentoRepository;
 import projeto.gerenciamentoMedicamentos.gerenciamentoMedicamentos.repositories.PacienteRepository;
 
 @RestController
@@ -17,9 +15,4 @@ public class PacienteController {
 	public List<Paciente> buscarPaciente() {
 		return new PacienteRepository().getListPaciente();
 	}
-<<<<<<< HEAD
 }
-=======
-
-}
->>>>>>> c6c41a8debd799044c7897871fe48ff07231199c
