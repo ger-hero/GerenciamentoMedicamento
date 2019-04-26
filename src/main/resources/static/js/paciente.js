1e4157e853cd2a0ea6bed386a2e82e79adff70ae
Andrei Dorneles Cruz <andrei.cruz@ibm.com>
2019-04-26 14:25:37 -0300

    referencia para novo html

--- src/main/resources/static/js/paciente.js
+++ src/main/resources/static/js/paciente.js
@@ -3,12 +3,12 @@
  */
 
 $(document).ready(function () {
-    $("#pesq").click(function (event) {
+//    $("#pesq").click(function (event) {
         //stop submit the form, we will post it manually.
         event.preventDefault();
         limparListaPacientes();
         fire_ajax_submit();
-    });
+//    });
 });
 
 function limparListaPacientes(){
