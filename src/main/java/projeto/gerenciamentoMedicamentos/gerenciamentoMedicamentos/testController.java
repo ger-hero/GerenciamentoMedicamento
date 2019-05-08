9569a935a2fe858d2d9a8251022eef6e16d22525
Henrique Ramires <31447784+hramires@users.noreply.github.com>
2019-05-08 15:42:49 -0300

    alterado metodo paciente classe testController

--- src/main/java/projeto/gerenciamentoMedicamentos/gerenciamentoMedicamentos/testController.java
+++ src/main/java/projeto/gerenciamentoMedicamentos/gerenciamentoMedicamentos/testController.java
@@ -1,7 +1,12 @@
 package projeto.gerenciamentoMedicamentos.gerenciamentoMedicamentos;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import projeto.gerenciamentoMedicamentos.gerenciamentoMedicamentos.model.Paciente;
+import projeto.gerenciamentoMedicamentos.gerenciamentoMedicamentos.repositories.PacienteRepository;
 
 @Controller
 public class testController {
@@ -26,8 +31,14 @@ public class testController {
 		return "html/pacientes";
 	}
 
-	@RequestMapping("/paciente")
-	public String paciente() {
-		return "html/paciente";
+//	@RequestMapping("/paciente")
+//	public String paciente() {
+//		return "html/paciente";
+//	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/paciente/{id}")
+	public String paciente(@PathVariable(value = "id") int id) {
+		return "html/paciente/"+id;
 	}
 }
