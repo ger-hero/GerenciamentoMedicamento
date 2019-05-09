28917f93c1d78b0bdd77c9ff1bbb58a3c09b5c9f
Andrei Dorneles Cruz <andrei.cruz@ibm.com>
2019-05-09 11:22:43 -0300

    alterado para pegar os dados de um paciente

--- src/main/resources/static/js/umPaciente.js
+++ src/main/resources/static/js/umPaciente.js
@@ -1,8 +1,15 @@
 $(document).ready(function () {
         event.preventDefault();
-       
+        listaPacienteId(getIdFromUrl());
 });
 
+
+function getIdFromUrl(){
+    var url = window.location.href;
+    var id = url.substring(url.lastIndexOf('/') + 1);
+    console.log(id);
+    return id;
+}
 
 function listaPacienteId(id) {
     $.ajax({
@@ -23,5 +30,23 @@ function listaPacienteId(id) {
 }
 
 function formatInformacao(json){
-	
-}
+	//alert(json.nome)
+	//console.log(json.nome);
+	panel = $('.panel .nome');
+	//panel-nome = $('.nome');
+	panel.html('<strong>Nome: </strong>' + json.nome);
+	//modalFooter.html('<a href="/paciente/' +  json.id + '\"' +  'class="btn btn-success"><i class="fas fa-edit"></i></a>');
+}	
+
+
+
+
+
+
+
+
+
+
+
+
+
