d448b9e246c5c7769ae0763e398a52bdde716f31
Henrique Ramires <31447784+hramires@users.noreply.github.com>
2019-05-10 14:18:57 -0300

    adicionado o metodo get doencas

--- src/main/java/projeto/gerenciamentoMedicamentos/gerenciamentoMedicamentos/repositories/PacienteRepository.java
+++ src/main/java/projeto/gerenciamentoMedicamentos/gerenciamentoMedicamentos/repositories/PacienteRepository.java
@@ -6,23 +6,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 import projeto.gerenciamentoMedicamentos.gerenciamentoMedicamentos.Conexao;
+import projeto.gerenciamentoMedicamentos.gerenciamentoMedicamentos.model.Doenca;
 import projeto.gerenciamentoMedicamentos.gerenciamentoMedicamentos.model.Paciente;
 
 public class PacienteRepository {
-
-	public PacienteRepository() {
-
-	}
 	
 	public Paciente getPaciente(int id) {
-		for(Paciente p : getListPaciente()) {
+		for(Paciente p : getListPacienteDoenca()) {
 			if(p.getId() == id)
 				return p;
 		}
 		return null;
 	}
-
-	public List<Paciente> getListPaciente() {
+	
+	public List<Paciente> getListPacienteDoenca() {
 		List<Paciente> pacientes = new ArrayList<Paciente>();
 		ResultSet resultSet;
 		resultSet = this.retornaPacientes();
@@ -39,15 +36,20 @@ public class PacienteRepository {
 				ala = resultSet.getString("ala");
 				foto = resultSet.getString("foto");
 				
-				pacientes.add(new Paciente(idPaciente, nomePaciente, ala, foto));
+				pacientes.add(new Paciente(idPaciente, nomePaciente, ala, foto, getDoencas(idPaciente)));
 			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 		return pacientes;
+	}
+	
+	public List<Doenca> getDoencas(int id) {
+		List<Doenca> doencas = new ArrayList<Doenca>();
+		doencas.add(new Doenca(1, "outra", "muito ruim", true, 1));
+		doencas.add(new Doenca(2, "doenca", "muito ruim", false, 2));
 
+		return doencas;
 	}
 
 	public ResultSet retornaPacientes() {
