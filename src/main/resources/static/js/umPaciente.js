35f714c45acf9add1c918eb3ef0cc0f74d66cbcf
Andrei Dorneles Cruz <andrei.cruz@ibm.com>
2019-05-10 22:59:37 -0300

    teste

--- src/main/resources/static/js/umPaciente.js
+++ src/main/resources/static/js/umPaciente.js
@@ -40,9 +40,9 @@ function formatInformacao(json){
 					'<div class="dados">' +
 						'<div class="nome"><strong>Nome: </strong>' + json.nome + '</div>' +
 						'<div class="ala"><strong>Ala: </strong>' + json.ala + '</div>' +
-					'</div>' +
-				'<div class="doencas">' + json.doencas[0].exemplo + '</div>' +
-				'</div>');
+					'</div>');
+				//'<div class="doencas">' + json.doencas[0].exemplo + '</div>' +
+				//'</div>');
 	//modalFooter.html('<a href="/paciente/' +  json.id + '\"' +  'class="btn btn-success"><i class="fas fa-edit"></i></a>');
 }	
 
