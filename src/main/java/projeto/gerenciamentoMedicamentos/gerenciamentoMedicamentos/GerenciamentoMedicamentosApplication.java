99fba829a3e3549abf9eda8214b0dd309ff27503
Henrique Ramires <31447784+hramires@users.noreply.github.com>
2019-05-21 15:50:50 -0300

    adicionado metodo heroku

--- src/main/java/projeto/gerenciamentoMedicamentos/gerenciamentoMedicamentos/GerenciamentoMedicamentosApplication.java
+++ src/main/java/projeto/gerenciamentoMedicamentos/gerenciamentoMedicamentos/GerenciamentoMedicamentosApplication.java
@@ -2,9 +2,18 @@ package projeto.gerenciamentoMedicamentos.gerenciamentoMedicamentos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class GerenciamentoMedicamentosApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	private String home() {
+		return "Hello World!";
+	}
+
 	public static void main(String[] args) {
 		DadosMemoria.getInstance();
 		SpringApplication.run(GerenciamentoMedicamentosApplication.class, args);
